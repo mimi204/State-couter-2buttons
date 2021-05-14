5c985b4a0d2edb2b9e4d8497c545c5f13858e327
Trabelsi Mariem <61707024+mimi204@users.noreply.github.com>
2021-05-14 12:06:31 +0000

    Update Counter.js

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -12,35 +12,6 @@ export class Counter extends Component {
             memory: null,
         }
     }
-
-    //third step in Mounting : methode componentdidmount()
-    // componentDidMount(){
-    //     console.log("componentDidMount()")
-    //     //methode js : setInterval , selon l'interval(1s) state change (+1)
-       
-    //     // const memory = setInterval(() => {
-    //     //     this.setState({ counter: this.state.counter+1 });
-    //         // }, 1000);
-    
-    //      this.setState({ memory:  setInterval(() => {
-    //         this.setState({ counter: this.state.counter+1 });
-    //     }, 1000) });
-    //     console.log(this.state.memory)
-    // }
-
-
-    //Second phase updating component
- //second step in Updating : methode componentDidUpdate()
-    componentDidUpdate() {
-        console.log("componentDidUpdate()")
-    }
-
-//third phase unmounting component
-    componentWillUnmount() {
-        console.log("componentWillUnmount()")
-        clearInterval(this.state.memory)
-    }
-
     increment = () => {
         this.setState({ counter:this.state.counter+1  });
     }
@@ -54,8 +25,6 @@ export class Counter extends Component {
         }
     }
     render() {
-       //second step in Mounting : methode render()
-   // first step in Updating: methode render()
 
         console.log("render()")
         return (
